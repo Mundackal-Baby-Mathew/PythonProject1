f3666b90a2e7ac6f7bc02e64c5c430bc375fd627
Mathews <immath5w@gmail.com>
2025-07-20 16:16:48 +0530

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.title("Hello Streamlit")
-st.right("Click a button below")
+st.write("Click a button below")
 if st.button("Say Hello"):
 	st.success("Hello World")
 if st.button("Say Good Bye"):
